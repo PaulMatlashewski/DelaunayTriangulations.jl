b73d47e5e8dcfae01a2d2026129073168e04215b
Paul Matlashewski <pmatlashewski@gmail.com>
2021-07-21 09:51:28 -0700

    faster kdtrees

--- test/kdtree.jl
+++ test/kdtree.jl
@@ -1,15 +1,20 @@
-using DelaunayTriangulations: KDTree, Node, sort_vertices, sort_values, value, node
+using DelaunayTriangulations:
+    KDTree,
+    Node,
+    sort_vertices,
+    sort_vertices!,
+    sort_values!
 
 function test_binary_search_property(tree, node_id, dim)
-    root = node(tree, node_id)
+    root = tree.nodes[node_id]
     if root.left != 0
-        left = node(tree, root.left)
-        @test value(tree, root.value, dim) >= value(tree, left.value, dim)
+        left = tree.nodes[root.left]
+        @test root.value[dim] >= left.value[dim]
         test_binary_search_property(tree, root.left, dim % 2 + 1)
     end
     if root.right != 0
-        right = node(tree, root.right)
-        @test value(tree, root.value, dim) < value(tree, right.value, dim)
+        right = tree.nodes[root.right]
+        @test root.value[dim] < right.value[dim]
         test_binary_search_property(tree, root.right, dim % 2 + 1)
     end
 end
@@ -18,21 +23,28 @@ end
     # Check edge case of one vertex in the tree
     vertices = [(rand(), rand())]
     tree = KDTree(vertices)
-    @test tree.values == vertices
-    @test tree.nodes == [Node(1, 0, 0)]
+    @test tree.nodes == [Node(vertices[1], 0, 0)]
     # Check binary search property
     tree = KDTree([(rand(), rand()) for _ in 1:10])
     test_binary_search_property(tree, 1, 1)
-    # Check permutation order
+    # Check sorting
     vertices = [(0.5185, 0.0347), (0.5216, 0.4219),
                 (0.6828, 0.9237), (0.4302, 0.4148),
                 (0.1816, 0.2890), (0.9312, 0.6561),
                 (0.5897, 0.3000), (0.8778, 0.0046),
                 (0.5463, 0.4520), (0.2716, 0.1995)]
-    nodes = [Node(1, 8, 2), Node(2, 3, 4), Node(8, 7, 0), Node(6, 5, 0), Node(9, 0, 6),
-             Node(3, 0, 0), Node(7, 0, 0), Node(4, 9, 0), Node(5, 0, 10), Node(10, 0, 0)]
-    tree = KDTree(vertices, nodes)
-    permutation = sort_values(tree)
-    @test permutation == [5, 10, 4, 1, 7, 8, 2, 9, 3, 6]
-    @test length(sort_vertices(vertices)) == length(permutation)
+    nodes = [Node(vertices[1], 8, 2), Node(vertices[2], 3, 4),
+             Node(vertices[8], 7, 0), Node(vertices[6], 5, 0),
+             Node(vertices[9], 0, 6), Node(vertices[3], 0, 0),
+             Node(vertices[7], 0, 0), Node(vertices[4], 9, 0),
+             Node(vertices[5], 0, 10), Node(vertices[10], 0, 0)]
+    tree = KDTree(nodes)
+    sorted_vertices = copy(vertices)
+    sort_values!(sorted_vertices, tree, 1, 1)
+    @test sorted_vertices == [vertices[5], vertices[10], vertices[4],
+                              vertices[1], vertices[7], vertices[8],
+                              vertices[2], vertices[9], vertices[3], vertices[6]]
+    sorted_vertices = sort_vertices(vertices)
+    sort_vertices!(vertices)
+    @test length(sorted_vertices) == length(vertices)
 end
